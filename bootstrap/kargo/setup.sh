7b0c353573d05a06e06db3dbd3457f1c55e1339a
Scott Yoder <syoder89@gmail.com>
2025-10-30 18:42:15 -0500

    Add github keys for Kargo

--- bootstrap/kargo/setup.sh
+++ bootstrap/kargo/setup.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+APP_DIR=../../applications/kargo-config
+helm dependency update ${APP_DIR}
+kubectl create namespace homelab-apps
+helm template -n homelab-apps kargo-config ${APP_DIR} \
+  | kubectl apply -f -
+
 kubectl apply -f manifests/
